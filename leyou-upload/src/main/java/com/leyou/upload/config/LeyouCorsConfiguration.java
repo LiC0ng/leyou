6d6af3b7dfe185db7b06a855b804bf2ac7d39ba7
LiC0ng <a1040484@gmail.com>
2020-01-30 16:24:49 +0900

    fix(upload): use FastDFS

--- leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
+++ leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.leyou.config;
+package com.leyou.upload.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ leyou-upload/src/test/java/com/leyou/upload/test/FastDFSTest.java
@@ -0,0 +1,54 @@
+package com.leyou.upload.test;
+
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.domain.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class FastDFSTest {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private ThumbImageConfig thumbImageConfig;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // 要上传的文件
+        File file = new File("C:\\Users\\LC\\Desktop\\1.jpg");
+        // 上传并保存图片， 参数: 1-上传的文件流 2-文件的大小 3-文件的后缀 4-可以不管他
+        StorePath storePath = this.storageClient.uploadFile(
+                new FileInputStream(file), file.length(), "jpg", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+    }
+
+    @Test
+    public void testUploadAndCreateThumb() throws FileNotFoundException {
+        // 要上传的文件
+        File file = new File("C:\\Users\\LC\\Desktop\\2.png");
+        // 上传并保存图片并生成缩略图， 参数: 1-上传的文件流 2-文件的大小 3-文件的后缀 4-可以不管他
+        StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
+                new FileInputStream(file), file.length(), "png", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+        // 获取缩略图路径
+        String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
+        System.out.println(path);
+    }
+}
