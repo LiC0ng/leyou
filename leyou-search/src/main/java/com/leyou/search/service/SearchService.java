5a46d923197a69222be625ec273adbdb8666da0a
LiC0ng <a1040484@gmail.com>
2020-02-04 22:00:27 +0900

    feat(search_goods): build goods instance from database
    add elastic search service, to build goods instance by spu id from database

---
+++ leyou-search/src/main/java/com/leyou/search/service/SearchService.java
@@ -0,0 +1,137 @@
+package com.leyou.search.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leyou.item.pojo.*;
+import com.leyou.search.client.BrandClient;
+import com.leyou.search.client.CategoryClient;
+import com.leyou.search.client.GoodsClient;
+import com.leyou.search.client.SpecificationClient;
+import com.leyou.search.pojo.Goods;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+public class SearchService {
+
+    @Autowired
+    private CategoryClient categoryClient;
+
+    @Autowired
+    private BrandClient brandClient;
+
+    @Autowired
+    private GoodsClient goodsClient;
+
+    @Autowired
+    private SpecificationClient specificationClient;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public Goods buildGoods(Spu spu) throws IOException {
+
+        Goods goods = new Goods();
+
+        // 根据分类id查询分类名称
+        List<String> names = this.categoryClient.queryNamesByIdS(Arrays.asList(spu.getCid1(), spu.getCid2(), spu.getCid3()));
+
+        // 根据品牌id查询品牌
+        Brand brand = this.brandClient.queryBrandById(spu.getBrandId());
+
+        // 根据cid3查询所有的搜索规格参数
+        List<SpecParam> params = this.specificationClient.queryParams(null, spu.getCid3(), null, true);
+        // 根据spuId查询spuDetail
+        SpuDetail spuDetail = this.goodsClient.querySpuDetailBySpuId(spu.getId());
+        // 把通用的规格参数值进行反序列化
+        Map<String, Object> genericSpecMap = MAPPER.readValue(spuDetail.getGenericSpec(), new TypeReference<Map<String, Object>>() {
+        });
+        // 把特殊的规格参数值进行反序列化
+        Map<String, List<Object>> specialSpecMap = MAPPER.readValue(spuDetail.getSpecialSpec(), new TypeReference<Map<String, List<Object>>>() {
+        });
+
+        Map<String, Object> specs = new HashMap<>();
+        params.forEach(param -> {
+            if (param.getGeneric()) { // 判断规格参数的类型是否是通用的规格参数
+                // 如果是通用类型的参数，从genericSpecMap获取规格参数值
+                String value = genericSpecMap.get(param.getId().toString()).toString();
+                // 判断是否是数值类型，如果是，则获取区间
+                if (param.getNumeric()) {
+                    value = chooseSegment(value, param);
+                }
+                specs.put(param.getName(), value);
+            } else {
+                // 如果是特殊类型的参数，从genericSpecMap获取规格参数值
+                String value = specialSpecMap.get(param.getId().toString()).toString();
+                specs.put(param.getName(), value);
+            }
+        });
+
+        // 根据spuId查询所有的sku
+        List<Sku> skus = this.goodsClient.querySkusBySpuId(spu.getId());
+        // 初始化一个价格集合，收集所有sku价格
+        List<Long> prices = new ArrayList<>();
+        // 收集sku的必要字段信息
+        List<Map<String, Object>> skuMapList = new ArrayList<>();
+        skus.forEach(sku -> {
+            prices.add(sku.getPrice());
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", sku.getId());
+            map.put("title", sku.getTitle());
+            map.put("price", sku.getPrice());
+            // 获取sku中的图片，数据库的图片可能是多张，以","进行分割，我们只取第一张
+            map.put("image", StringUtils.isBlank(sku.getImages()) ? " " : StringUtils.split(sku.getImages(), ",")[0]);
+            skuMapList.add(map);
+        });
+
+        goods.setId(spu.getId());
+        goods.setCid1(spu.getCid1());
+        goods.setCid2(spu.getCid2());
+        goods.setCid3(spu.getCid3());
+        goods.setBrandId(spu.getBrandId());
+        goods.setCreateTime(spu.getCreateTime());
+        goods.setSubtitle(spu.getSubTitle());
+        // 拼接all字段，需要分类名称和品牌名称
+        goods.setAll(spu.getTitle() + " " + StringUtils.join(names, ' ') + " " + brand.getName());
+        // 获取spu下的所有sku的价格
+        goods.setPrice(prices);
+        // 获取spu下的所有sku，并转化成json
+        goods.setSkus(MAPPER.writeValueAsString(skuMapList));
+        // 获取所有查询的规格参数{name:value}
+        goods.setSpecs(specs);
+
+        return goods;
+    }
+
+    private String chooseSegment(String value, SpecParam p) {
+        double val = NumberUtils.toDouble(value); // 任何数值都可转为double，故在此用double更通用
+        String result = "其它";
+        // 保存数值段
+        for (String segment : p.getSegments().split(",")) {
+            String[] segs = segment.split("-");
+            // 获取数值范围
+            double begin = NumberUtils.toDouble(segs[0]);
+            double end = Double.MAX_VALUE;
+            if (segs.length == 2) {
+                end = NumberUtils.toDouble(segs[1]);
+            }
+            // 判断是否在范围内
+            if (val >= begin && val < end) {
+                if (segs.length == 1) {
+                    result = segs[0] + p.getUnit() + "以上";
+                } else if (begin == 0) {
+                    result = segs[1] + p.getUnit() + "以下";
+                } else {
+                    result = segment + p.getUnit();
+                }
+                break;
+            }
+        }
+        return result;
+    }
+}
